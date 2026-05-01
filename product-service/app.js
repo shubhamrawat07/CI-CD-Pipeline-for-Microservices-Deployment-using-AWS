63dd1ee300e0dfe705d304af155faa4bb5506004
Shubham Rawat <rawatji2210@gmail.com>
2026-05-01 15:14:29 +0000

    2 changes

--- product-service/app.js
+++ product-service/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Product Test 3 Running");
+    res.send("Product Test 4 Running");
 });
 
 app.listen(3000, ()=>{
